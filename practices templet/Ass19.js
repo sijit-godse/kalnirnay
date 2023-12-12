02927fb88f6a08e75943267c5f47d4b44d50ae1d
sijit-godse <godse.sv@gmail.com>
2023-12-12 16:07:51 +0530

    commit 19

---
+++ practices templet/Ass19.js
@@ -0,0 +1,6 @@
+<script>
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+</script>
